e50f1047bb1aab4bdbfae889ffa2a8866e67f3d7
andjelkadzida <andjelkadzida@gmail.com>
2023-03-26 18:47:44 +0200

    Update test.

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -1,27 +1,26 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:vodic_kroz_valjevo/main.dart';
-import 'package:vodic_kroz_valjevo/localization/supported_languages.dart';
 import 'package:vodic_kroz_valjevo/pages/home_page.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 
 void main() {
   group('VodicKrozValjevo', () {
     testWidgets('should set language', (WidgetTester tester) async {
-      await tester.pumpWidget(MaterialApp(
+      await tester.pumpWidget(const MaterialApp(
         localizationsDelegates: AppLocalizations.localizationsDelegates,
         supportedLocales: AppLocalizations.supportedLocales,
         home: VodicKrozValjevo(),
       ));
       VodicKrozValjevo.setLanguage(
-          tester.element(find.byType(VodicKrozValjevo)), Locale('en'));
+          tester.element(find.byType(VodicKrozValjevo)), const Locale('en'));
       await tester.pumpAndSettle();
       expect(find.byType(HomePage), findsOneWidget);
     });
 
     testWidgets('should use default language if not set',
         (WidgetTester tester) async {
-      await tester.pumpWidget(MaterialApp(
+      await tester.pumpWidget(const MaterialApp(
         localizationsDelegates: AppLocalizations.localizationsDelegates,
         supportedLocales: AppLocalizations.supportedLocales,
         home: VodicKrozValjevo(),
@@ -29,7 +28,7 @@ void main() {
       await tester.pumpAndSettle();
       expect(
           Localizations.localeOf(tester.element(find.byType(VodicKrozValjevo))),
-          equals(Locale('en')));
+          equals(const Locale('en')));
     });
   });
 }

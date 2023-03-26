18b841225079d2c13879222ecfc1dff5094b1e00
andjelkadzida <andjelkadzida@gmail.com>
2023-03-26 17:39:16 +0200

    Make existing widgets accessible.

--- lib/navigation/navigation_drawer.dart
+++ lib/navigation/navigation_drawer.dart
@@ -32,30 +32,45 @@ class NavigationDrawer extends StatelessWidget {
                 Expanded(
                   child: Column(
                     children: [
-                      DrawerItem(
-                        name: localization(context).homePage,
-                        icon: Icons.home,
-                        onPressed: () => onItemPressed(context, index: 0),
+                      Semantics(
+                        label: localization(context).homePage,
+                        child: DrawerItem(
+                          name: localization(context).homePage,
+                          icon: Icons.home,
+                          onPressed: () => onItemPressed(context, index: 0),
+                        ),
                       ),
-                      DrawerItem(
-                        name: localization(context).aboutCity,
-                        icon: Icons.info,
-                        onPressed: () => onItemPressed(context, index: 1),
+                      Semantics(
+                        label: localization(context).aboutCity,
+                        child: DrawerItem(
+                          name: localization(context).aboutCity,
+                          icon: Icons.info,
+                          onPressed: () => onItemPressed(context, index: 1),
+                        ),
                       ),
-                      DrawerItem(
-                        name: localization(context).sights,
-                        icon: Icons.location_city,
-                        onPressed: () => onItemPressed(context, index: 2),
+                      Semantics(
+                        label: localization(context).sights,
+                        child: DrawerItem(
+                          name: localization(context).sights,
+                          icon: Icons.location_city,
+                          onPressed: () => onItemPressed(context, index: 2),
+                        ),
                       ),
-                      DrawerItem(
-                        name: localization(context).sportRecreation,
-                        icon: Icons.sports_gymnastics,
-                        onPressed: () => onItemPressed(context, index: 3),
+                      Semantics(
+                        label: localization(context).sportRecreation,
+                        child: DrawerItem(
+                          name: localization(context).sportRecreation,
+                          icon: Icons.sports_gymnastics,
+                          onPressed: () => onItemPressed(context, index: 3),
+                        ),
                       ),
-                      DrawerItem(
-                        name: localization(context).restaurantsAndHotels,
-                        icon: Icons.restaurant_menu,
-                        onPressed: () => onItemPressed(context, index: 4),
+                      Semantics(
+                        label: localization(context).restaurantsAndHotels,
+                        child: DrawerItem(
+                          name: localization(context).restaurantsAndHotels,
+                          icon: Icons.restaurant_menu,
+                          onPressed: () => onItemPressed(context, index: 4),
+                        ),
                       ),
                     ],
                   ),
@@ -105,28 +120,34 @@ class NavigationDrawer extends StatelessWidget {
 
   //Header widget
   Widget headerWidget(BuildContext buildContext) {
-    return Row(
-      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-      children: [
-        Text(
-          localization(buildContext).cityName,
-          style: const TextStyle(fontSize: 25, color: Colors.white),
-        ),
-        Padding(
-          padding: const EdgeInsets.all(1),
-          child: languageWidget(buildContext),
-        ),
-      ],
+    return Semantics(
+      label: localization(buildContext).cityName,
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        children: [
+          Text(
+            localization(buildContext).cityName,
+            style: const TextStyle(fontSize: 25, color: Colors.white),
+          ),
+          Padding(
+            padding: const EdgeInsets.all(1),
+            child: languageWidget(buildContext),
+          ),
+        ],
+      ),
     );
   }
 
   // Language dropdown widget
   Widget languageWidget(BuildContext buildContext) {
     return DropdownButton2<Language>(
-      icon: const Icon(
-        Icons.language,
-        color: Colors.white,
-        textDirection: TextDirection.ltr,
+      icon: Semantics(
+        label: localization(buildContext).languageMenu,
+        child: const Icon(
+          Icons.language,
+          color: Colors.white,
+          textDirection: TextDirection.ltr,
+        ),
       ),
       underline: const SizedBox(),
       hint: Text(localization(buildContext).language),
@@ -146,11 +167,19 @@ class NavigationDrawer extends StatelessWidget {
               child: Row(
                 mainAxisAlignment: MainAxisAlignment.spaceAround,
                 children: <Widget>[
-                  Text(
-                    e.flag,
-                    style: const TextStyle(fontSize: 20),
+                  Semantics(
+                    label: e.flag,
+                    child: Text(
+                      e.flag,
+                      style: const TextStyle(fontSize: 20),
+                    ),
                   ),
-                  Text(e.name)
+                  Semantics(
+                    label: e.name,
+                    child: Text(
+                      e.name,
+                    ),
+                  ),
                 ],
               ),
             ),

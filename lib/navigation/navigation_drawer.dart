7caf0b01ecd901d41a74ea5163b68a9eaaf1107e
andjelkadzida <andjelkadzida@gmail.com>
2023-12-05 23:44:19 +0100

    Navigation optimization.

--- lib/navigation/navigation_drawer.dart
+++ lib/navigation/navigation_drawer.dart
@@ -1,9 +1,7 @@
-import 'package:dropdown_button2/dropdown_button2.dart';
 import 'package:flutter/material.dart';
 import 'package:vodic_kroz_valjevo/localization/language.dart';
 import 'package:vodic_kroz_valjevo/localization/supported_languages.dart';
 import 'package:vodic_kroz_valjevo/main.dart';
-import 'package:vodic_kroz_valjevo/navigation/drawer_item.dart';
 import 'package:vodic_kroz_valjevo/pages/about_city.dart';
 import 'package:vodic_kroz_valjevo/pages/home_page.dart';
 import 'package:vodic_kroz_valjevo/pages/hotels_and_restaurants.dart';
@@ -30,47 +28,38 @@ class NavigationDrawer extends StatelessWidget {
                   color: Colors.grey,
                 ),
                 Expanded(
-                  child: Column(
+                  child: ListView(
+                    shrinkWrap: true,
                     children: [
-                      Semantics(
+                      drawerItemWidget(
+                        context,
                         label: localization(context).homePage,
-                        child: DrawerItem(
-                          name: localization(context).homePage,
-                          icon: Icons.home,
-                          onPressed: () => onItemPressed(context, index: 0),
-                        ),
+                        icon: Icons.home,
+                        index: 0,
                       ),
-                      Semantics(
+                      drawerItemWidget(
+                        context,
                         label: localization(context).aboutCity,
-                        child: DrawerItem(
-                          name: localization(context).aboutCity,
-                          icon: Icons.info,
-                          onPressed: () => onItemPressed(context, index: 1),
-                        ),
+                        icon: Icons.info,
+                        index: 1,
                       ),
-                      Semantics(
+                      drawerItemWidget(
+                        context,
                         label: localization(context).sights,
-                        child: DrawerItem(
-                          name: localization(context).sights,
-                          icon: Icons.location_city,
-                          onPressed: () => onItemPressed(context, index: 2),
-                        ),
+                        icon: Icons.location_city,
+                        index: 2,
                       ),
-                      Semantics(
+                      drawerItemWidget(
+                        context,
                         label: localization(context).sportRecreation,
-                        child: DrawerItem(
-                          name: localization(context).sportRecreation,
-                          icon: Icons.sports_gymnastics,
-                          onPressed: () => onItemPressed(context, index: 3),
-                        ),
+                        icon: Icons.sports_gymnastics,
+                        index: 3,
                       ),
-                      Semantics(
+                      drawerItemWidget(
+                        context,
                         label: localization(context).restaurantsAndHotels,
-                        child: DrawerItem(
-                          name: localization(context).restaurantsAndHotels,
-                          icon: Icons.restaurant_menu,
-                          onPressed: () => onItemPressed(context, index: 4),
-                        ),
+                        icon: Icons.restaurant_menu,
+                        index: 4,
                       ),
                     ],
                   ),
@@ -83,7 +72,28 @@ class NavigationDrawer extends StatelessWidget {
     );
   }
 
-  //Routing navigation
+  // Routing navigation
+  Widget drawerItemWidget(BuildContext context,
+      {required String label, required IconData icon, required int index}) {
+    return Semantics(
+      label: label,
+      button: true,
+      child: ListTile(
+        title: Text(label,
+            style: const TextStyle(
+                fontSize: 20,
+                color: Colors.white,
+                fontFamily: 'Roboto',
+                fontWeight: FontWeight.w500)),
+        leading: Icon(
+          icon,
+          color: Colors.white, // Set icon color to white
+        ),
+        onTap: () => onItemPressed(context, index: index),
+      ),
+    );
+  }
+
   void onItemPressed(BuildContext context, {required int index}) {
     Navigator.pop(context);
 
@@ -118,81 +128,74 @@ class NavigationDrawer extends StatelessWidget {
     }
   }
 
-  //Header widget
+  // Header widget
   Widget headerWidget(BuildContext buildContext) {
     return Semantics(
       label: localization(buildContext).cityName,
-      child: Row(
-        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-        children: [
-          Text(
-            localization(buildContext).cityName,
-            style: const TextStyle(fontSize: 25, color: Colors.white),
-          ),
-          Padding(
-            padding: const EdgeInsets.all(1),
-            child: languageWidget(buildContext),
-          ),
-        ],
+      child: Container(
+        width: MediaQuery.of(buildContext).size.width,
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            Expanded(
+              child: Text(
+                localization(buildContext).cityName,
+                style: const TextStyle(
+                    fontSize: 25,
+                    color: Colors.white,
+                    fontFamily: 'Roboto',
+                    fontWeight: FontWeight.w700),
+              ),
+            ),
+            languageWidget(buildContext),
+          ],
+        ),
       ),
     );
   }
 
   // Language dropdown widget
   Widget languageWidget(BuildContext buildContext) {
-    return MouseRegion(
-      cursor: SystemMouseCursors.click,
-      child: Tooltip(
-        message: localization(buildContext).languageMenu,
-        child: DropdownButton2<Language>(
-          icon: Semantics(
-            label: localization(buildContext).languageMenu,
-            child: const Icon(
-              Icons.language,
-              color: Colors.white,
-              textDirection: TextDirection.ltr,
-            ),
-          ),
-          underline: const SizedBox(),
-          dropdownDecoration: null,
-          hint: Text(localization(buildContext).language),
-          onChanged: (Language? language) async {
-            if (language != null) {
-              Locale locale = await setLocale(language.scriptCode);
-              VodicKrozValjevo.setLanguage(buildContext, locale);
-            }
-          },
-          dropdownDirection: DropdownDirection.left,
-          scrollbarAlwaysShow: false,
-          style: const TextStyle(
-              fontSize: 15, fontFamily: 'Roboto', color: Colors.black),
-          items: Language.languageList()
-              .map<DropdownMenuItem<Language>>(
-                (e) => DropdownMenuItem<Language>(
-                  value: e,
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceAround,
-                    children: <Widget>[
-                      Semantics(
-                        label: e.flag,
-                        child: Text(
-                          e.flag,
-                          style: const TextStyle(fontSize: 20),
-                        ),
-                      ),
-                      Semantics(
-                        label: e.name,
-                        child: Text(
-                          e.name,
-                        ),
-                      ),
-                    ],
-                  ),
-                ),
-              )
-              .toList(),
+    return PopupMenuButton<Language>(
+      tooltip: localization(buildContext).languageMenu, // Tooltip for the icon
+      icon: Semantics(
+        label: localization(buildContext).languageMenu,
+        child: const Icon(
+          Icons.language,
+          color: Colors.white,
+          textDirection: TextDirection.ltr,
         ),
       ),
+      offset: const Offset(60, 40), // Adjust the vertical offset as needed
+      onSelected: (Language language) async {
+        Locale locale = await setLocale(language.scriptCode);
+        VodicKrozValjevo.setLanguage(buildContext, locale);
+      },
+      itemBuilder: (BuildContext context) {
+        return Language.languageList().map((Language e) {
+          return PopupMenuItem<Language>(
+            value: e,
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
+              children: <Widget>[
+                Semantics(
+                  label: e.flag,
+                  child: Text(
+                    e.flag,
+                    style: const TextStyle(fontSize: 20),
+                  ),
+                ),
+                Semantics(
+                  label: e.name,
+                  child: Text(
+                    e.name,
+                  ),
+                ),
+              ],
+            ),
+          );
+        }).toList();
+      },
     );
   }
 }

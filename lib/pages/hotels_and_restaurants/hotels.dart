3c669533f567461ce9c2062b22388f6ee5d30344
andjelkadzida <andjelkadzida@gmail.com>
2024-01-13 18:10:15 +0100

    Added alert dialog on marker click.

--- lib/pages/hotels_and_restaurants/hotels.dart
+++ lib/pages/hotels_and_restaurants/hotels.dart
@@ -1,61 +1,64 @@
+import 'dart:typed_data';
+
 import 'package:flutter/material.dart';
 import 'package:flutter_map/flutter_map.dart';
 import 'package:latlong2/latlong.dart';
-import 'package:sqflite/sqflite.dart';
 
 import '../../database_config/database_helper.dart';
 import '../../localization/supported_languages.dart';
 import '../../styles/common_styles.dart';
 
 class Hotels extends StatelessWidget {
-  const Hotels({Key? key}) : super(key: key);
-
-  Future<List<LatLng>> fetchMarkerData() async {
-    final db = await DatabaseHelper.instance.getNamedDatabase();
-    final List<Map<String, dynamic>> maps =
-        await db.rawQuery('SELECT latitude, longitude FROM Hotels');
-
-    return List.generate(maps.length, (i) {
-      return LatLng(
-        maps[i]['latitude'] as double,
-        maps[i]['longitude'] as double,
-      );
-    });
-  }
+  Hotels({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     final textScaler = MediaQuery.textScalerOf(context);
 
     return Scaffold(
-        appBar: AppBar(
-            title: Semantics(
-                label: localization(context).hotels,
-                child: Text(
-                  localization(context).hotels,
-                  style: AppStyles.defaultAppBarTextStyle(textScaler),
-                )),
-            excludeHeaderSemantics: true,
-            centerTitle: true,
-            backgroundColor: Colors.black,
-            iconTheme:
-                const IconThemeData(color: Colors.white) // Color of drawer icon
-            ),
-        body: FutureBuilder<List<LatLng>>(
-            future: fetchMarkerData(),
-            builder: (context, snapshot) {
-              return DatabaseHelper.buildFutureState<List<LatLng>>(
-                  context: context,
-                  snapshot: snapshot,
-                  onData: (data) => buildWithMarkers(data));
-            }));
+      appBar: AppBar(
+        title: Semantics(
+            label: localization(context).hotels,
+            child: Text(
+              localization(context).hotels,
+              style: AppStyles.defaultAppBarTextStyle(textScaler),
+            )),
+        excludeHeaderSemantics: true,
+        centerTitle: true,
+        backgroundColor: Colors.black,
+        iconTheme: const IconThemeData(color: Colors.white),
+      ),
+      body: FutureBuilder<List<Map<String, dynamic>>>(
+        future: _getHotelsDataFromDatabase(localization(context).localeName),
+        builder: (context, snapshot) {
+          return DatabaseHelper.buildFutureState<List<Map<String, dynamic>>>(
+            context: context,
+            snapshot: snapshot,
+            onData: (data) => buildWithMarkers(context, data),
+          );
+        },
+      ),
+    );
   }
 
-  Widget buildWithMarkers(List<LatLng> markerData) {
-    List<Marker> markers = markerData
-        .map((latLng) =>
-            Marker(point: latLng, child: const Icon(Icons.pin_drop)))
-        .toList();
+  Widget buildWithMarkers(
+      BuildContext context, List<Map<String, dynamic>> hotelsData) {
+    List<Marker> markers = hotelsData.map((hotelData) {
+      LatLng position = LatLng(
+          hotelData['latitude'] as double, hotelData['longitude'] as double);
+
+      return Marker(
+        point: position,
+        child: IconButton(
+          icon: const Icon(
+            Icons.pin_drop,
+            size: 20,
+          ),
+          color: Colors.black,
+          onPressed: () => showHotelDetailsDialog(context, hotelData),
+        ),
+      );
+    }).toList();
 
     return FlutterMap(
       options: const MapOptions(
@@ -72,19 +75,48 @@ class Hotels extends StatelessWidget {
     );
   }
 
+  void showHotelDetailsDialog(
+      BuildContext context, Map<String, dynamic> hotelData) {
+    final textScaler = MediaQuery.textScalerOf(context);
+    showDialog(
+        context: context,
+        builder: (BuildContext context) {
+          Uint8List imageBytes = hotelData['hotel_image_path'];
+          return AlertDialog(
+            title: Text(hotelData['title'],
+                style: AppStyles.sightTitleStyle(textScaler)),
+            content: Column(
+              mainAxisSize: MainAxisSize.min,
+              children: <Widget>[
+                Image.memory(
+                  imageBytes,
+                  fit: BoxFit.contain,
+                ),
+                Text('Stars: ${hotelData['noStars']}'),
+              ],
+            ),
+            actions: <Widget>[
+              TextButton(
+                child: Text('Close'),
+                onPressed: () => Navigator.of(context).pop(),
+              ),
+            ],
+          );
+        });
+  }
+
   Future<List<Map<String, dynamic>>> _getHotelsDataFromDatabase(
       String languageCode) async {
-    final Database db = await DatabaseHelper.instance.getNamedDatabase();
+    final db = await DatabaseHelper.instance.getNamedDatabase();
 
     final List<Map<String, dynamic>> data = await db.rawQuery('''
       SELECT 
-        hotels_image_path, 
-        title_$languageCode AS title,
-        latitude,
-        longitude,
-        noStars
-      FROM 
-        Hotels
+        hotel_image_path, 
+        title_$languageCode AS title, 
+        latitude, 
+        longitude, 
+        noStars 
+      FROM Hotels
     ''');
     return data;
   }
